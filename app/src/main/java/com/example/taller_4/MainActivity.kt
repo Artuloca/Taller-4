5ee41e10dc5e99ad9ea52623f1479f1a5c19f8ad
Arturo <artuloca@myuax.co>
2024-11-19 18:35:05 +0100

    pantalla inicial v2

--- app/src/main/java/com/example/taller_4/MainActivity.kt
+++ app/src/main/java/com/example/taller_4/MainActivity.kt
@@ -3,12 +3,19 @@ package com.example.taller_4
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.navigation.NavController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
 import com.example.taller_4.ui.theme.Taller4Theme
 import java.util.Calendar
 
@@ -17,8 +24,10 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             Taller4Theme {
-                Surface(color = MaterialTheme.colorScheme.background) {
-                    Greeting()
+                val navController = rememberNavController()
+                NavHost(navController = navController, startDestination = "greeting") {
+                    composable("greeting") { GreetingScreen(navController) }
+                    composable("next") {  }
                 }
             }
         }
@@ -26,23 +35,37 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(modifier: Modifier = Modifier) {
+fun GreetingScreen(navController: NavController) {
     val currentHour = Calendar.getInstance().get(Calendar.HOUR_OF_DAY)
     val greetingText = when (currentHour) {
         in 0..11 -> "Good Morning"
         in 12..17 -> "Good Afternoon"
         else -> "Good Evening"
     }
-    Text(
-        text = "$greetingText!",
-        modifier = modifier
-    )
+
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(
+            text = "$greetingText!",
+            fontSize = 32.sp,
+            fontWeight = FontWeight.Bold,
+            modifier = Modifier.padding(bottom = 16.dp)
+        )
+        Button(onClick = { navController.navigate("next") }) {
+            Text(text = "Siguiente")
+        }
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun GreetingScreenPreview() {
     Taller4Theme {
-        Greeting()
+        GreetingScreen(rememberNavController())
     }
 }
